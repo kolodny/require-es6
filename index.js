f7d76d19e7cb48c09bc703ffeb0eb6c094adb838
moshek <mkolodny@integralads.com>
2014-09-02 10:39:42 -0400

    inital commit

---
+++ index.js
@@ -0,0 +1,16 @@
+var fs = require('fs');
+
+var oldRequire = require;
+
+module.exports = function(pkg) {
+  try {
+    var meta = oldRequire(pkg + '/package.json');
+    var mainEs6 = meta['main-es6'];
+    if (mainEs6) {
+      return oldRequire.call(this, pkg.replace(/\/$/, '') + '/' + mainEs6.replace(/^\//, ''));
+    }
+  } catch (e) {
+    console.warn('there was some issue with ' + pkg);
+  }
+  return oldRequire.call(this, pkg);
+};
